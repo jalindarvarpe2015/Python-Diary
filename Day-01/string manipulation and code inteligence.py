f5794ccc702a67e80581f85f01c9568f79f2a314
Jalindar.Varpe@external.t-systems.com <Jalindar.Varpe@external.t-systems.com>
2024-05-02 15:39:50 +0530

    adding new content of string and string cocatination

--- Day-01/string manipulation and code inteligence.py
+++ Day-01/string manipulation and code inteligence.py
@@ -1,1 +1,44 @@
-print("Hellow world! \n Hellow world!\n Hellow world ")
+# String Manipulatin and code inteligence 
+
+'''
+String is a collection of alphabets, words or other characters.
+Python has a built-in string class named str
+Python strings are "immutable" which means they cannot be changed after they are created.
+'''
+
+
+print("Hellow world!\n Hellow world!\n  Hellow world")
+
+'''
+Hellow world!
+ Hellow world!
+  Hellow world
+
+'''
+
+print("Hellow world!\nHellow world!\nHellow world")
+
+'''
+Hellow world!
+Hellow world!
+Hellow world
+
+'''
+
+# string concatination
+
+
+print("helow" + "jalindar")
+# helowjalindar
+
+print("helow " + "jalindar")
+# helow jalindar
+
+print("helow" + " jalindar")
+# helow jalindar
+
+print("helow" + " " + "jalindar")
+# helow jalindar
+
+print("helow" + " @ " + "jalindar")
+# helow @ jalindar
