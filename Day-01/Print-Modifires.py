9ae89219df84f035056eeb3c75e5ac14887ae5cc
Jalindar.Varpe@external.t-systems.com <Jalindar.Varpe@external.t-systems.com>
2024-05-02 15:22:20 +0530

    adding output of print statements

--- Day-01/Print-Modifires.py
+++ Day-01/Print-Modifires.py
@@ -5,11 +5,17 @@
 # Print Modifires 
 
 print('she said : "hellow" and then left')
+#output -  she said : "hellow" and then left
 
 print("she said : \"Hellow\" and then left ")
+# she said : "Hellow" and then left 
+
 
 print("A 'single quote' inside a double quote")
+# A 'single quote' inside a double quote
 
 print('A "double quote" inside a single quote')
+# A "double quote" inside a single quote
 
 print("Alternatively you can just \"escape\" the quote")
+# Alternatively you can just "escape" the quote
