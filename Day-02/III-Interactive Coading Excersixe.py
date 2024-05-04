24b376f79a2a7dc38731136371a990e27149a1bb
Jalindar.Varpe@external.t-systems.com <Jalindar.Varpe@external.t-systems.com>
2024-05-04 19:03:09 +0530

    adding new changes

---
+++ Day-02/III-Interactive Coading Excersixe.py
@@ -0,0 +1,6 @@
+#Problem Statement
+
+#Write a program that adds the digit in a 2 digit number . e.g . If the input was 35, then the  output should be 8 (like 3+5)
+
+#Example input 35
+#Example output 8
