63dd4990a5a3f3d2daaec7de29096861a807715d
Jalindar.Varpe@external.t-systems.com <Jalindar.Varpe@external.t-systems.com>
2024-05-04 18:52:58 +0530

    adding new chanhes

---
+++ Day-02/I-primitive_Data Types.py
@@ -0,0 +1,28 @@
+# primitive Data Types
+
+# Data Types
+
+print("helloe"[3])
+# l
+
+print(123 + 456)
+# 579
+
+print("123" + "456")
+# 123456
+
+# large Interger
+
+print(123_456_789)
+#  123456789
+
+# float 
+
+print(123.35)
+print(type(123.35))
+#<class 'float'>
+
+
+# booolean
+- True
+- False
