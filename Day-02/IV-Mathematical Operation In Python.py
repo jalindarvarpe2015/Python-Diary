a892b18145f46039eabb930cc234228bcca37b0c
Jalindar.Varpe@external.t-systems.com <Jalindar.Varpe@external.t-systems.com>
2024-05-05 16:33:01 +0530

    adding new changes

---
+++ Day-02/IV-Mathematical Operation In Python.py
@@ -0,0 +1,31 @@
+print(3 + 5 )
+print(3 - 5) 
+print(3 * 2)
+print(6 / 3)
+
+'''
+8
+-2
+6
+2.0  -----> always float numbern
+
+'''
+
+print(type(6 /3 ))
+
+# <class 'float'>
+
+print(2 ** 3)
+# 8
+
+'''
+order of execution 
+
+parenthesis
+exponents
+multiplication
+division
+addition 
+subtraction.
+
+'''
