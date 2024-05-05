bebc2ab66181f411bc95cf041b93144c6f74c461
Jalindar.Varpe@external.t-systems.com <Jalindar.Varpe@external.t-systems.com>
2024-05-05 17:19:58 +0530

    Adding new changes

---
+++ Day-02/VI-Number Manipulation and F Strings in Python.py
@@ -0,0 +1,16 @@
+# Number Manipulation and F strings in Python
+
+print(8 / 3)
+# 2.6666666666666665
+
+print(int(8/3))
+# 2
+
+print(round(8/3))
+# 3
+
+print(round(8/3, 2))
+# 2.67
+
+print(round(2.666666665, 2))
+# 2.67
