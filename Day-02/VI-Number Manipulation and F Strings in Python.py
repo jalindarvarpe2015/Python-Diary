e845e75760b577363784890761f1929d6c87c08c
Jalindar.Varpe@external.t-systems.com <Jalindar.Varpe@external.t-systems.com>
2024-05-05 17:29:31 +0530

    adding new changes

--- Day-02/VI-Number Manipulation and F Strings in Python.py
+++ Day-02/VI-Number Manipulation and F Strings in Python.py
@@ -14,3 +14,35 @@ print(round(8/3, 2))
 
 print(round(2.666666665, 2))
 # 2.67
+
+print(8 // 3)
+# 2
+
+print(type(8//3))
+# <class 'int'>
+
+print(4/2)
+# 2.0
+
+print(type(4/2))
+# <class 'float'>
+
+print(4//2)
+# 2
+
+print(type(4//2))
+#2
+#<class 'int'>
+
+
+
+score = 0
+
+# user score a point 
+
+score += 1         # score = score + 1
+print(score)
+# 1
+
+
+# F strings
