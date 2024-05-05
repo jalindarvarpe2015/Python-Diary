fc7ac12b23ea9e6e197f8dfc8cdb05420d2d8306
Jalindar.Varpe@external.t-systems.com <Jalindar.Varpe@external.t-systems.com>
2024-05-05 17:13:10 +0530

    adding bmi code

---
+++ Day-02/V-BMI Calcualtor.py
@@ -0,0 +1,31 @@
+'''
+BMI - Body Mass Index
+
+Write a program that calcualte the body mass index (BMI) From a user weight and height 
+
+BMI = Weight(kg)/height(m²)
+
+Note: - you should convert the bmi to a whole number and print out a 
+whole number in order to pass all the tests
+
+Example Input :
+1.75
+80
+
+means : height 80 and height 1.75
+
+
+Example Output 
+26 
+
+since : 80 + (1.75 * 1.75) = 26.1224
+
+'''
+
+height = float(input("Enter your Height : "))
+
+weight = int(input("Enter ypur Height : "))
+
+BMI = weight / (height * height) 
+
+print("Your Body Mass Index (BMI) IS ", BMI)
