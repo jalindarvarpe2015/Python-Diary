5558a18a5cb1e5553fd8c37620ba9cc0fdf1008f
Jalindar.Varpe@external.t-systems.com <Jalindar.Varpe@external.t-systems.com>
2024-05-06 10:52:13 +0530

    ADDING NEW CHANGES

--- Day-03/I-Control low with _if.py
+++ Day-03/I-Control low with _if.py
@@ -1,6 +1,6 @@
 print("Wellcome to the rollercoaster")
 
-height = int(input("What is your height in cm" ))
+height = int(input("What is your height in cm ? " ))
 
 if height > 120:
     print("you can ride the rollercoaster")
