44f36423ba294ec397abd980c77a3443ff58a44b
Jalindar.Varpe@external.t-systems.com <Jalindar.Varpe@external.t-systems.com>
2024-05-06 11:11:08 +0530

    adding new chnages

---
+++ Day-03/II-Interactive Excersice.py
@@ -0,0 +1,12 @@
+'''
+write a program that works out whether if a given number is an odd or even number 
+
+'''
+
+number = int(input("Enter a number "))
+
+
+if number %2==0:
+    print("This is even number")
+else:
+    print("This is odd number")
