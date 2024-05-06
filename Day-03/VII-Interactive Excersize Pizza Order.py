c029b789e688e1368f039b1d241d6ca89ae0321d
Jalindar.Varpe@external.t-systems.com <Jalindar.Varpe@external.t-systems.com>
2024-05-06 17:26:50 +0530

    adding new chanhes

---
+++ Day-03/VII-Interactive Excersize Pizza Order.py
@@ -0,0 +1,23 @@
+'''
+INSTRUCTION
+
+Congratulations, you have got a job at Python Pizza! Your first job is to build an automatic pizza order program
+
+based on users order , work out their final bill
+
+small size(S) : 15   RS
+Medium Pizza(M) : 20 RS
+Large Pizza(L)  : 25 RS
+
+Add pepperoni for small pizza ( Y or N) : 2 RS
+Add pepperoni for medium large pizza ( Y or N ) : 3 RS
+Add extra cheese for any size pizza ( Y or N )  : 1
+
+Example Input 
+L
+Y
+N
+
+Example Output
+
+'''
