e29af05fe236de822a110a7593cf3937329b014e
Jalindar.Varpe@external.t-systems.com <Jalindar.Varpe@external.t-systems.com>
2024-05-06 11:03:00 +0530

    adding new chanhgess

--- Day-03/I-Control low with _if.py
+++ Day-03/I-Control low with _if.py
@@ -6,3 +6,11 @@ if height >= 120:
     print("you can ride the rollercoaster")
 else:
     print("sorry, you have to grow taller before you can ride")
+    
+    
+'''
+= ---->  This is the assignment operator. It is used to assign a value to a variable
+== --->   This is the equality operator. It is used to compare whether two values are equal
+!= ---> not equal to 
+
+'''
