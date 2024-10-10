43b884ff6233f1b14ba8b13d8bbabfd0668fd9c3
Jalindar Varpe <126875635+jalindarvarpe2015@users.noreply.github.com>
2024-10-10 12:46:34 +0530

    Create List comprehension.py
    Adding New file

---
+++ List comprehension.py
@@ -0,0 +1,1 @@
+List comprehension in Python is a concise way to create lists. It allows you to generate a new list by applying an expression to each item in an existing iterable (like a list, tuple, or string). The general syntax is
