b0143ffa18ee10069ed1857f2b96d5fa4b698485
Jalindar Varpe <126875635+jalindarvarpe2015@users.noreply.github.com>
2024-10-28 15:36:20 +0530

    Create Nested_List.py
    nested_list in python

---
+++ Nested_List.py
@@ -0,0 +1,28 @@
+# Nested List 
+
+A nested list in Python is essentially a list that contains other lists as its elements.
+This structure allows you to create multi-dimensional data representations, which can be useful for various applications like matrices, tables, or complex data arrangements.
+
+  A nested list is just a list Within the list
+
+# Nested lidt example 
+nested_list = [[1,2,3],[4,5],[6,7,8]]
+
+# flattening the nested list using function
+
+def listObjectId(rolesArray):
+  values = []
+  for i in rolesArray:
+    for j in i:
+      values.append(j)
+    return values
+
+flat_list = listObjectId(nested_list)
+print(flat_list)
+
+
+#output = [1,2,3,4,5,6,7,8,9]
+
+
+
+
